a17f927f8760754b661f258c5bb59372dd7cbfbc
SaatwikCodz <107233799+SaatwikCodz@users.noreply.github.com>
2023-01-05 07:54:42 +0530

    Adding titles

--- Front End/gallery.py
+++ Front End/gallery.py
@@ -5,6 +5,7 @@ set_appearance_mode('dark')
 set_default_color_theme('blue')
 
 showroom = CTk()
+showroom.title("Showroom")
 img = CTkImage(light_image = Image.open("C:/Users/Saatwik Coding/Desktop/GitHub/Public-Code/Images/Item-1.png"),
 			   dark_image = Image.open("C:/Users/Saatwik Coding/Desktop/GitHub/Public-Code/Images/Item-1.png"),
 			   size = (300, 150)
--- Front End/promo.py
+++ Front End/promo.py
@@ -5,6 +5,7 @@ set_appearance_mode('dark')
 set_default_color_theme('blue')
 
 promo = CTk()
+promo.title("Please Enter your CODE")
 e = CTkEntry(promo, placeholder_text = "PROMO CODE HERE")
 e.pack()
 b = CTkButton(promo, text = "Verify").pack()
