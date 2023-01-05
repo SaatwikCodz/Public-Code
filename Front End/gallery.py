26b40438674d4ce5222b3ff1dd334763d6f13971
SaatwikCodz <107233799+SaatwikCodz@users.noreply.github.com>
2023-01-05 07:59:04 +0530

    donne

--- Front End/gallery.py
+++ Front End/gallery.py
@@ -11,5 +11,6 @@ img = CTkImage(light_image = Image.open("C:/Users/Saatwik Coding/Desktop/GitHub/
 			   size = (300, 150)
 	)
 lbl = CTkLabel(showroom, image = img, text = "").pack()
-btn = CTkButton(showroom, text = "Apply Promo", fg_color = "transparent").pack()
+btn = CTkButton(showroom, text = "Apply Promo", fg_color = "transparent", border_color = "cyan", border_width = 3).pack()
+showroom.attributes('-fullscreen', True)
 showroom.mainloop()
